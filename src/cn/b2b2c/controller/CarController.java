6585c197659de7a6881e35b88adb4d8259b9e3bb
xiejingyang888 <39820960+xiejingyang888@users.noreply.github.com>
2018-07-16 15:41:04 +0800

    谢

--- src/cn/b2b2c/controller/CarController.java
+++ src/cn/b2b2c/controller/CarController.java
@@ -10,7 +10,6 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.websocket.Session;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
