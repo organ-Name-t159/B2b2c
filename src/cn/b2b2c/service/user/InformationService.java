b0dfd3c75e3607505eae066e4b2570c04179f408
3408629835 <39799111+3408629835@users.noreply.github.com>
2018-07-23 13:52:47 +0800

    wyq

---
+++ src/cn/b2b2c/service/user/InformationService.java
@@ -0,0 +1,12 @@
+package cn.b2b2c.service.user;
+
+import cn.b2b2c.pojo.User;
+
+public interface InformationService {
+	/**
+	 * 修改基本信息
+	 * @param user
+	 * @return
+	 */
+	 public int update (User user);
+}
