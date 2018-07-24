dde71ea4abb6647c25047f2b63fa694b5d0ca307
xiejingyang888 <39820960+xiejingyang888@users.noreply.github.com>
2018-07-24 17:36:12 +0800

    xie

--- src/cn/b2b2c/controller/UserController.java
+++ src/cn/b2b2c/controller/UserController.java
@@ -1,23 +1,28 @@
 package cn.b2b2c.controller;
 
 
-<<<<<<< HEAD
 
+
+
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 
-=======
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
->>>>>>> 1716a29267fabdd35e06386be55fff0ade0a09e9
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import cn.b2b2c.pojo.DiscountCoupon;
 import cn.b2b2c.pojo.User;
+import cn.b2b2c.pojo.UserAddress;
+import cn.b2b2c.service.discountcoupon.DiscountCouponService;
 import cn.b2b2c.service.user.UserService;
 import cn.b2b2c.tools.TimeTransform;
 
@@ -27,14 +32,15 @@ import cn.b2b2c.tools.TimeTransform;
 @Controller
 @RequestMapping("/user")
 public class UserController {
-<<<<<<< HEAD
+
 	
 
 
-=======
+
 	@Autowired
 	private UserService  userService;
->>>>>>> 1716a29267fabdd35e06386be55fff0ade0a09e9
+@Autowired
+private  DiscountCouponService dcs;
 	
 	@RequestMapping(value="/welocome.html")
 	public String welocome(HttpSession session)throws Exception {
@@ -44,14 +50,12 @@ public class UserController {
 	
 	@RequestMapping(value="/userMessage.html")
 	public String userMessage(HttpSession session,HttpServletRequest request)throws Exception {		
-<<<<<<< HEAD
-		
-=======
+
 		User user = (User) session.getAttribute("user");		
 		user=userService.basic(user);
 		user.setCommB(TimeTransform.isTime(user.getBirthday()).toString());
 		request.setAttribute("useryou", user);
->>>>>>> 1716a29267fabdd35e06386be55fff0ade0a09e9
+
 		return "user/UserMessage";
 	}
 	
@@ -62,6 +66,49 @@ public class UserController {
 		return "user/AccountSecurity";
 	}
 	
+	@RequestMapping(value="/BindEmail.html")
+	public String accountBindEmail(HttpSession session)throws Exception {
+				
+		return "user/BindEmail";
+	}
 	
+	@RequestMapping(value="/BindEmailOne.html")
+	public String accountBindEmailOne(HttpSession session)throws Exception {
+				
+		return "user/BindEmailOne";
+	}
+
+	@RequestMapping(value="/BindEmailTow.html")
+	public String accountBindEmailTow(HttpSession session)throws Exception {
+				
+		return "user/BindEmailTow";
+	}
+	
+	@RequestMapping(value="/BindEmailSuccess.html")
+	public String accountBindEmailSuccess(HttpSession session)throws Exception {
+				
+		return "user/BindEmailSuccess";
+	}
+	
+	@RequestMapping(value="/RedEnvelopes.html")
+	public String RedEnvelopes(HttpSession session,HttpServletRequest request)throws Exception {
+			DiscountCoupon coupon=new DiscountCoupon();
+		Integer userId=Integer.parseInt(request.getParameter("userId"));
+		
+		List<DiscountCoupon> dcStateList=dcs.getQueryById(userId);
+		
+		for (DiscountCoupon discountCoupon : dcStateList) {
+			discountCoupon.setNewTime(TimeTransform.isTime(discountCoupon.getValidTime()).toString());
+		}
+		session.setAttribute("dcStateList", dcStateList);
+		return "user/RedEnvelopes";
+	}
+	@RequestMapping("ReceivingAddress.html")
+	public String ReceivingAddress(HttpSession session) throws Exception {
+		User user=(User)session.getAttribute("user");
+		List<UserAddress> addressesList=userService.getAll(user.getId());
+		session.setAttribute("addressesList", addressesList);
+		return "user/ReceivingAddress";
+	}
 	
 }
