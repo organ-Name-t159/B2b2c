7520a0a39e35cd3822ef75166d200280e15b432d
xiejingyang888 <39820960+xiejingyang888@users.noreply.github.com>
2018-07-25 18:37:14 +0800

    xie

--- src/cn/b2b2c/controller/UserController.java
+++ src/cn/b2b2c/controller/UserController.java
@@ -17,6 +17,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import cn.b2b2c.pojo.DiscountCoupon;
 import cn.b2b2c.pojo.User;
@@ -103,12 +105,59 @@ private  DiscountCouponService dcs;
 		session.setAttribute("dcStateList", dcStateList);
 		return "user/RedEnvelopes";
 	}
-	@RequestMapping("ReceivingAddress.html")
+	//查询收货地址
+	@RequestMapping(value="ReceivingAddress.html")
 	public String ReceivingAddress(HttpSession session) throws Exception {
 		User user=(User)session.getAttribute("user");
 		List<UserAddress> addressesList=userService.getAll(user.getId());
 		session.setAttribute("addressesList", addressesList);
 		return "user/ReceivingAddress";
 	}
+	//修改收货地址
+	@RequestMapping(value="UpdateAddress",method=RequestMethod.POST)
+	@ResponseBody
+	public String UpdateAddress(HttpServletRequest request) throws Exception {
+		User user=(User)request.getSession().getAttribute("user");
+			String consignee=request.getParameter("consignee");
+			String email=request.getParameter("email");
+			String address=request.getParameter("address");
+			Integer postcode=Integer.parseInt(request.getParameter("zipcode")) ;
+			String addressPhone=request.getParameter("mobile");
+			Integer id=Integer.parseInt(request.getParameter("id"));
+			System.out.println("=========================================");
+			System.out.println(id+" "+user.getId());
+			int num=userService.update(id, user.getId(), consignee, address, addressPhone, postcode);
+			if(num==1) {
+				return "200";
+			}
+		
+		
+		return "errer";
+	}
+	//删除收货地址
+	@RequestMapping(value="DelAddress")
+	public String DelAddress(HttpServletRequest request) throws Exception{
+		User user=(User)request.getSession().getAttribute("user");
+		String i=request.getParameter("uid");
+		System.out.println(i+"====================================");
+		Integer id=Integer.parseInt(i);
+		System.out.println(id+"======================");
+		int num=userService.deleteAddress(user.getId(), id);
+		if (num==1) {
+			return "redirect:ReceivingAddress.html";
+		}
+		return "error";
+	}
 	
+	//新增收货地址
+	@RequestMapping(value="addAddress",method=RequestMethod.POST)
+	public String addAddress(HttpServletRequest request) throws Exception {
+		User user=(User)request.getSession().getAttribute("user");
+		String consignee=request.getParameter("consignee");
+		String address=request.getParameter("address");
+		String addressPhone=request.getParameter("mobile");
+		int postcode=Integer.parseInt(request.getParameter("zipcode"));
+		int num=userService.addAddress(user.getId(), consignee, address, addressPhone, postcode);
+		return "redirect:ReceivingAddress.html";
+	}
 }
